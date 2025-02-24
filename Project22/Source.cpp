c35c38dc3e3c200f890c587b4090e9bb4d205d89
CodeOtakuu <notbukaaa777@gmail.com>
2025-02-24 13:45:15 +0400

    GDI to Direct2D

--- Project22/Source.cpp
+++ Project22/Source.cpp
@@ -1,171 +1,127 @@
 ﻿#include <windows.h>
+#include <d2d1.h>
 #include <math.h>
 #include <iostream>
-#include <sstream>
+
+#pragma comment(lib, "d2d1")
 
 #define ID_TIMER 1
-#define FORCE 50 //def50
+#define FORCE 50 
 
 int wh = 500;
 int ww = 500;
 
 struct Circle {
-    int x, y, radius;
-    float vx, vy; 
-    float gravity;  
-    float bounce;  
+    float x, y, radius;
+    float vx, vy;
+    float gravity;
+    float bounce;
 };
 
-Circle circle = { 200, 200, 50, 0, 0, 0.5f, 0.8f }; 
+Circle circle = { 200, 200, 50, 0, 0, 0.5f, 0.8f };
 Circle Usil = { 500, 300, 30, 0, 0, 0 };
 
-void DrawCircle(HDC hdc, Circle& circle) {
-    Ellipse(hdc, circle.x - circle.radius, circle.y - circle.radius,
-        circle.x + circle.radius, circle.y + circle.radius);
+ID2D1Factory* pFactory = NULL;
+ID2D1HwndRenderTarget* pRenderTarget = NULL;
+ID2D1SolidColorBrush* pBrush = NULL;
+
+void DrawCircle(ID2D1HwndRenderTarget* pRT, ID2D1SolidColorBrush* pBrush, Circle& circle) {
+    pRT->FillEllipse(D2D1::Ellipse(D2D1::Point2F(circle.x, circle.y), circle.radius, circle.radius), pBrush);
 }
 
-void SpeedUpIfHit(Circle& obj, Circle& target) {
-   
-    float dx = obj.x - target.x;
-    float dy = obj.y - target.y;
-    float distance = sqrt(dx * dx + dy * dy);  
+void InitD2D(HWND hwnd) {
+    D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &pFactory);
+    RECT rc;
+    GetClientRect(hwnd, &rc);
+    D2D1_SIZE_U size = D2D1::SizeU(rc.right - rc.left, rc.bottom - rc.top);
 
-  
-    if (distance < obj.radius + target.radius) {
-        obj.vy = 500;  
-        obj.vx = 1000; 
-    }
+    pFactory->CreateHwndRenderTarget(
+        D2D1::RenderTargetProperties(),
+        D2D1::HwndRenderTargetProperties(hwnd, size),
+        &pRenderTarget);
+
+    pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::White), &pBrush);
 }
-
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-
-    std::wstringstream ss; 
-
     switch (msg) {
     case WM_PAINT:
     {
-
-
-
         PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-
-        HDC hdcBuffer = CreateCompatibleDC(hdc);
-        HBITMAP hbmBuffer = CreateCompatibleBitmap(hdc, ww, wh);
-        HBITMAP hbmOld = (HBITMAP)SelectObject(hdcBuffer, hbmBuffer);
-    
-        ss.str(L""); 
-        ss << L"Position: (" << circle.x << L", " << circle.y << L")\n";
-        ss << L"Velocity: (" << circle.vx << L", " << circle.vy << L")\n";
-        ss << L"Bounce Coefficient: " << circle.bounce << L"\n";
-        ss << L"Gravity: " << circle.gravity << L"\n";
-
-       
-        if (circle.vy < 0) {
-            ss << L"Bounce Direction: Upward\n";
-        }
-        else if (circle.vy > 0) {
-            ss << L"Bounce Direction: Downward\n";
-        }
-
-       
-        if (circle.vx < 0) {
-            ss << L"Moving: Left\n";
-        }
-        else if (circle.vx > 0) {
-            ss << L"Moving: Right\n";
-        }
-        else {
-            ss << L"Moving: Stationary (Horizontal)\n";
-        }
-
-        DrawCircle(hdcBuffer, circle);
-        DrawCircle(hdcBuffer, Usil);
-        SetBkColor(hdcBuffer, RGB(0, 0, 0));
-        SetTextColor(hdcBuffer, RGB(0, 255, 0));
-        TextOut(hdcBuffer, 10, 10, ss.str().c_str(), ss.str().length());
-    
-
-        BitBlt(hdc, 0, 0, ww, wh, hdcBuffer, 0, 0, SRCCOPY);
-
-        SelectObject(hdcBuffer, hbmOld);
-        DeleteObject(hbmBuffer);
-        DeleteDC(hdcBuffer);
-   
+        BeginPaint(hwnd, &ps);
+        pRenderTarget->BeginDraw();
+        pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::Black));
+        DrawCircle(pRenderTarget, pBrush, circle);
+        //DrawCircle(pRenderTarget, pBrush, Usil);
+        pRenderTarget->EndDraw();
         EndPaint(hwnd, &ps);
     }
     break;
 
     case WM_SIZE:
 
-        ww = LOWORD(lParam);  
-        wh = HIWORD(lParam);
-
-
-
+        if (pRenderTarget) {
+            RECT rc;
+            GetClientRect(hwnd, &rc);
+            D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+            pRenderTarget->Resize(size);
+        }
         break;
 
     case WM_TIMER:
-    {
-
-
 
         circle.vy += circle.gravity;
 
-        
         circle.x += (int)circle.vx;
         circle.y += (int)circle.vy;
-
-     
         RECT rect;
+
         GetClientRect(hwnd, &rect);
         if (circle.x - circle.radius < 0) {
             circle.x = circle.radius;
-            circle.vx = -circle.vx * circle.bounce;  
+            circle.vx = -circle.vx * circle.bounce;
         }
         if (circle.x + circle.radius > rect.right) {
             circle.x = rect.right - circle.radius;
-            circle.vx = -circle.vx * circle.bounce; 
+            circle.vx = -circle.vx * circle.bounce;
         }
 
-      
+
         if (circle.y - circle.radius < 0) {
             circle.y = circle.radius;
-            circle.vy = -circle.vy * circle.bounce;  
+            circle.vy = -circle.vy * circle.bounce;
         }
         if (circle.y + circle.radius > rect.bottom) {
             circle.y = rect.bottom - circle.radius;
-            circle.vy = -circle.vy * circle.bounce; 
+            circle.vy = -circle.vy * circle.bounce;
         }
 
-        SpeedUpIfHit(circle, Usil);
-
-        InvalidateRect(hwnd, NULL, TRUE);
-    }
-    break;
-
-    case WM_CREATE:
-        SetTimer(hwnd, ID_TIMER, 1, NULL);
-        break;
-
-    case WM_KEYDOWN:
-        if (wParam == VK_LEFT) circle.vx = -FORCE;  
-        if (wParam == VK_RIGHT) circle.vx = FORCE; 
-        if (wParam == VK_UP) circle.y -= FORCE;
-        if (wParam == VK_DOWN) circle.y += FORCE;
-
-        RECT rect;
-        GetClientRect(hwnd, &rect);
         if (circle.x - circle.radius < 0) circle.x = circle.radius;
         if (circle.x + circle.radius > rect.right) circle.x = rect.right - circle.radius;
         if (circle.y - circle.radius < 0) circle.y = circle.radius;
         if (circle.y + circle.radius > rect.bottom) circle.y = rect.bottom - circle.radius;
 
-        InvalidateRect(hwnd, NULL, TRUE);
+        InvalidateRect(hwnd, NULL, FALSE);
+        break;
+
+    case WM_CREATE:
+        InitD2D(hwnd);
+        SetTimer(hwnd, ID_TIMER, 1, NULL);
+        break;
+
+    case WM_KEYDOWN:
+        if (wParam == VK_LEFT) circle.vx += -FORCE;
+        if (wParam == VK_RIGHT) circle.vx += FORCE;
+        if (wParam == VK_UP) circle.vy += -FORCE;
+        if (wParam == VK_DOWN) circle.vy += FORCE;
+        InvalidateRect(hwnd, NULL, FALSE);
         break;
 
     case WM_DESTROY:
+        KillTimer(hwnd, ID_TIMER);
+        if (pBrush) pBrush->Release();
+        if (pRenderTarget) pRenderTarget->Release();
+        if (pFactory) pFactory->Release();
         PostQuitMessage(0);
         break;
 
@@ -176,22 +132,22 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 }
 
 int main() {
-
-    WNDCLASS wc = { 0 };
+    WNDCLASS wc = {};
     wc.lpfnWndProc = WndProc;
     wc.hInstance = GetModuleHandle(NULL);
     wc.lpszClassName = L"CircleApp";
     wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
 
-    if (!RegisterClass(&wc)) return -1;
+    RegisterClass(&wc);
 
-    HWND hwnd = CreateWindow(wc.lpszClassName, L"Circle App", WS_OVERLAPPEDWINDOW,
+    HWND hwnd = CreateWindow(wc.lpszClassName, L"Direct2D Circle App", WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, ww, wh, NULL, NULL, wc.hInstance, NULL);
+
     if (!hwnd) return -1;
 
     ShowWindow(hwnd, SW_SHOW);
 
-    MSG msg;
+    MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
